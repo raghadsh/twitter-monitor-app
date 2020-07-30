32a3f341931f14c250874107615e8006a2d69d5e
rsalshalan <rsalshalan@gmail.com>
2020-07-30 09:16:15 +0300

    update apis

--- flaskapp.py
+++ flaskapp.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask import  Flask, jsonify, request
 from flask_restful import Api, Resource, reqparse
+
 app = Flask(__name__)
+CORS(app)
+api = Api(app)
+
 @app.route('/')
 def hello_world():
   return 'Hello from new flask !'
+
+
 if __name__ == '__main__':
   app.run()

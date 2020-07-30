9e9a71a813bd1af5a370f3bc1df30f2fc379f955
rsalshalan <rsalshalan@gmail.com>
2020-07-30 08:58:03 +0300

    add apis

--- flaskapp.py
+++ flaskapp.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource, reqparse
 app = Flask(__name__)
-@app.route('/')
-def hello_world():
-  return 'Hello from Flask!'
+CORS(app)
+api = Api(app)
+
+
+class Get_Trends(Resource):
+    def get(self):
+        return  "test"
+
+#os.system("java -cp fgw.jar farasa.gateway.LemmatizerEntryPoint")
+api.add_resource(Get_Trends, "/Get_Trends/")
+
+
+
 if __name__ == '__main__':
   app.run()
+

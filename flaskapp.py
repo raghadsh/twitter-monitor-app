7965ec882521831051f37f8c0b82302fd14b58ad
rsalshalan <rsalshalan@gmail.com>
2020-07-30 09:23:45 +0300

    add trends

--- flaskapp.py
+++ flaskapp.py
@@ -5,11 +5,6 @@ from flask_restful import Api, Resource, reqparse
 app = Flask(__name__)
 api = Api(app)
 
-@app.route('/')
-def hello_world():
-  return 'Hello from new flask !'
-
-
 class Get_Trends(Resource):
     def get(self):
         return  "trends"
